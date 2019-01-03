fcdc76143b55c8e0b21eff29c947e8df130ccb03
Dasol Park <dasol.park615@gmail.com>
2019-01-03 17:28:16 +0900

    ExpressJS 2.3 Your First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+function handleListening() {
+  console.log("Listening on: http://localholst:4000");
+}
+
+app.listen(4000, handleListening);
