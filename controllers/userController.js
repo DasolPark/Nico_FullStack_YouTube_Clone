df677583787d7b7f9a83da3447ad16cbd845ce1a
Dasol Park <dasol.park615@gmail.com>
2019-01-10 16:36:22 +0800

    ExpressJS 2.24 Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ import routes from "../routes";
 export const getJoin = (req, res) => {
   res.render("Join", { pageTitle: "Join" });
 };
-
 export const postJoin = (req, res) => {
   const {
     body: { name, email, password, password2 }
@@ -18,10 +17,14 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("Login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
-  res.render("Logout", { pageTitle: "Logout" });
-export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+  res.render("Logout", { pageTitle: "Log Out" });
 export const userDetail = (req, res) =>
   res.render("UserDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
