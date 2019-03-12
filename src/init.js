9be5ca94ecaf5866d41e0640d21ae8b210e14bf3
Dasol Park <dasol.park615@gmail.com>
2019-03-12 21:14:12 +0800

    Config_variables Modified2

--- src/init.js
+++ src/init.js
@@ -12,6 +12,7 @@ import "./models/User";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+  // console.log(`Listening on: http://localhost:${PORT}`);
+  console.log("Listening on: https://safe-cliffs-48550.herokuapp.com/");
 
 app.listen(PORT, handleListening);
